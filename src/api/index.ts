fc0bc524a3313af3323ecb76f54149c35a8d2984
BlackH3art <jaroslaw.musielak@outlook.com>
2022-05-19 22:07:27 +0200

    fix: https in api link

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const API: AxiosInstance = axios.create({
-  baseURL: 'http://api.nbp.pl/api/exchangerates/rates/a/', 
+  baseURL: 'https://api.nbp.pl/api/exchangerates/rates/a/', 
 });
 
 export const getPrice = (ticker: string) => API.get(ticker);
